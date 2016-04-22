b09c0c7885c72eb6eeb77195d04900cc0b61fe99
jellebens <jellebens@outlook.com>
2016-04-22 20:00:08 +0200

    First version does not send events

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
+'use strict';
+
 var mosca = require('mosca');
 var winston = require('winston')
+var mqttDevice = require('azure-iot-device-mqtt');
+var iotDevice = require('azure-iot-device');
+var config = require('./config');
 
 var logger = new (winston.Logger)({transports : [new (winston.transports.Console)({'timestamp': true})]});
 
-var moscaSettings = {port: 1833};
+var moscaSettings = {port: config.mosca.port };
 
 var server = new mosca.Server(moscaSettings)
+
+var protocol = mqttDevice.Mqtt;
+
+var client = mqttDevice.clientFromConnectionString(config.iothub.connectionstring, protocol);
+
+client.open(function(err){
+	if(err){
+		logger.info('Could not connect to azure iothub ' + err);
+	}else{
+		logger.info('Connected to azure hub');
+	}
+});
 
 server.on('ready', function(){
 	logger.info('Mosca Server started');
@@ -16,5 +33,29 @@ server.on('clientConnected', function(client){
 });
 
 server.on('published', function(packet, client){
-	logger.info('Received', packet.payload);
+	logger.info('published to topic', packet.topic.toString());
+
+	 var deviceId = packet.topic.toString().split('/', 1).toString();
+	 var topic = packet.topic.toString().split('/')[1].toString();
+	 var data = JSON.stringify({ "payload": packet.payload.toString(), "topic": topic, "DeviceId": deviceId, "TimeStamp": Date() });
+	 var message = new iotDevice.Message(data);
+
+	if(client){
+
+	client.sendEvent(message, function(err){
+		if(err) {
+			logger.error(err.toString());
+		}
+	});
+}
+
+
 });
+
+function print(op){
+	return function printResult(err, res) {
+        if (err) console.log('IOT: ' + op + ' error: ' + err.toString());
+        if (res && (res.statusCode !== 204)) console.log('IOT: ' + op + ' status: ' + res.statusCode + ' ' + res.statusMessage);
+    };
+
+}
