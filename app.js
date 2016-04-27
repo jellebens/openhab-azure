8f4f3171e48d19c964b1b00096605f5e49b153f5
jellebens <jellebens@outlook.com>
2016-04-27 18:47:17 +0200

    first crude version

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var config = require('./config');
 
 var logger = new (winston.Logger)({transports : [new (winston.transports.Console)({'timestamp': true})]});
 
-var moscaSettings = {port: config.mosca.port };
+var moscaSettings = { port: config.mosca.port };
 
 var server = new mosca.Server(moscaSettings)
 
@@ -21,7 +21,7 @@ var connectCallback = function (err) {
   if (err) {
       logger.error('Could not connect: ' + err.message);
         } else {
-	    logger.info('Client connected');
+	    logger.info('Client connected to azure');
 	    client.on('message', function (msg) {
 	    logger.Info('Id: ' + msg.messageId + ' Body: ' + msg.data);
 	   
@@ -48,19 +48,13 @@ server.on('clientConnected', function(client){
 server.on('published', function(packet, aClient){
 	logger.info('published to topic', packet.topic.toString());
 
-	 var deviceId = packet.topic.toString().split('/', 1).toString();
 	 var topic = packet.topic.toString().split('/')[1].toString();
-	 var data = JSON.stringify({ "payload": packet.payload.toString(), "topic": topic, "DeviceId": deviceId, "TimeStamp": Date() });
-	 var message = new iotDevice.Message(data);
-
-	if(client){
-		client.sendEvent(message, printResultFor('send'));
 	
-	}else{
-		logger.warn('No client element found skipping');
+	 var data = JSON.stringify({ "payload": packet.payload.toString(), "topic": topic, "DeviceId": config.iothub.deviceId, "TimeStamp": Date() });
+	 if(topic == 'openhab'){	
+	    var message = new iotDevice.Message(data);
+	    client.sendEvent(message, printResultFor('send'));
 	}
-
-
 });
 
 var authenticate = function(client, username, password, callback){
@@ -79,8 +73,13 @@ var authorizeSubscribe = function (client, topic, callback) {
  
 function printResultFor(op){
 	return function printResult(err, res) {
-        if (err) console.log('IOT: ' + op + ' error: ' + err.toString());
-        if (res && (res.statusCode !== 204)) console.log('IOT: ' + op + ' status: ' + res.statusCode + ' ' + res.statusMessage);
+         if(err){ 
+		logger.error('IOT: ' + op + ' error: ' + err.toString());
+	}
+        
+		
+
+	
     };
 
 }
